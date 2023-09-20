cca904bc0e71a3c52095205ef3164318d56cccaf
yudiatmoko <yudiatmokoilham@gmail.com>
2023-09-20 23:43:51 +0700

    Revert "Create countin button"
    This reverts commit aafcd2550c4f8f664c771d5151b630eaf98c9b5d.

--- app/src/main/java/com/jaws/challengeappilham/presentation/fragmentmenudetail/FragmentMenuDetail.kt
+++ app/src/main/java/com/jaws/challengeappilham/presentation/fragmentmenudetail/FragmentMenuDetail.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.jaws.challengeappilham.R
 import com.jaws.challengeappilham.databinding.FragmentMenuDetailBinding
 import com.jaws.challengeappilham.model.Menu
 
@@ -18,7 +17,10 @@ class FragmentMenuDetail : Fragment() {
         FragmentMenuDetailArgs.fromBundle(arguments as Bundle).menu
     }
 
-    var count: Int = 0
+    companion object{
+        const val ADD_TO_CART = "Tambah ke Keranjang - "
+        const val RP = "Rp."
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,45 +34,18 @@ class FragmentMenuDetail : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         showProfileData()
-        countingClickListener()
-    }
-
-    private fun countingClickListener() {
-        binding.icAdd.setOnClickListener{
-            incrementCount()
-        }
-        binding.icRemove.setOnClickListener{
-            decrementCount()
-        }
-    }
-
-    private fun decrementCount() {
-        count--
-        if(count<=1){
-            count=1;
-        }
-        binding.tvAmount.setText(count.toString())
-        val total = menu?.menuPrice?.toInt()!! * count
-        binding.btnAddToCart.setText(getString(R.string.add_to_cart,total))
-    }
-
-    private fun incrementCount() {
-        count++
-        binding.tvAmount.setText(count.toString())
-        val total = menu?.menuPrice?.toInt()!! * count
-        binding.btnAddToCart.setText(getString(R.string.add_to_cart,total))
     }
 
     private fun showProfileData() {
         if(menu != null){
             binding.ivImgMenuItemDetail.setImageResource(menu?.menuImg!!)
             binding.tvMenuName.text = menu?.menuName
-            binding.tvMenuPrice.setText(getString(R.string.rupiah, menu?.menuPrice?.toInt()))
-            binding.btnAddToCart.setText(getString(R.string.add_to_cart, menu?.menuPrice?.toInt()))
+            binding.tvMenuPrice.text = "$RP ${menu?.menuPrice?.toInt()}"
+            binding.btnAddToCart.text = "$ADD_TO_CART $RP ${menu?.menuPrice?.toInt()}"
             binding.tvMenuDesc.text = menu?.menuDesc
 
         } else{
-            Toast.makeText(requireContext(), "Menu is null", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Profile is null", Toast.LENGTH_SHORT).show()
         }
     }
 }
