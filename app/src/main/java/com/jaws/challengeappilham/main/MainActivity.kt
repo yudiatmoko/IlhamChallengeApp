a861d970b66b02a9534b6957418e032296028535
yudiatmoko <yudiatmokoilham@gmail.com>
2023-09-26 17:39:08 +0700

    add bottom navigation

--- app/src/main/java/com/jaws/challengeappilham/main/MainActivity.kt
+++ app/src/main/java/com/jaws/challengeappilham/main/MainActivity.kt
@@ -2,6 +2,10 @@ package com.jaws.challengeappilham.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jaws.challengeappilham.R
 import com.jaws.challengeappilham.databinding.ActivityMainBinding
 
 
@@ -14,5 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
 }
