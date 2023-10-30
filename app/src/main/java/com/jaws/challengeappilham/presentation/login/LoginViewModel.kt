f55a7c0d1b242eaf912ed873500a0f3b96fafdc2
yudiatmoko <96367661+yudiatmoko@users.noreply.github.com>
2023-10-30 22:32:40 +0700

    Update LoginViewModel.kt

--- app/src/main/java/com/jaws/challengeappilham/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/jaws/challengeappilham/presentation/login/LoginViewModel.kt
@@ -6,13 +6,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jaws.challengeappilham.data.repository.UserRepository
 import com.jaws.challengeappilham.utils.ResultWrapper
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@HiltViewModel
-class LoginViewModel @Inject constructor(private val repo: UserRepository) : ViewModel() {
+class LoginViewModel(private val repo: UserRepository) : ViewModel() {
 
     private val _loginResult = MutableLiveData<ResultWrapper<Boolean>>()
 
